b3b76c9d4efe6e9e69ba2ce3ac6ca79bbb79e86f
Xhoana Shkajoti <@XhoanaShkajoti>
2025-01-23 00:01:29 +0100

    added functions

--- s328928.py
+++ s328928.py
@@ -1,61 +1,41 @@
 import numpy as np
 
-
-#PASTE HERE THE sXXXXX.py FILE SO WE CAN TEST IT BEFORE SUBMITTING IT
-
-
-# f0 is provided by the professor
-def f0(x: np.ndarray) -> np.ndarray:
-    return 0
-
-
-
-
-def f1(x: np.ndarray) -> np.ndarray: 
-    return 0
+def f1(x: np.ndarray) -> np.ndarray:
+    return np.sin(x[0])
 
 
 def f2(x: np.ndarray) -> np.ndarray: 
-    return 0
+   return np.multiply(np.multiply(np.add(np.add(np.add(-62.24357039260542, np.multiply(x[0], x[1])), np.add(np.multiply(x[0], x[1]), np.subtract(np.add(np.cbrt(np.subtract(np.multiply(x[0], x[1]), 4.978572892293439)), np.cbrt(np.subtract(np.multiply(x[0], x[1]), 1.9122493176103035))), -7.117004066355557))), np.multiply(np.multiply(5.3708717652722076, x[0]), np.reciprocal(np.divide(1.6472744402097064, x[2])))), np.add(np.subtract(np.multiply(x[1], 4.978572892293439), np.divide(np.multiply(7.980474713921989, x[2]), -1.641003402164408)), np.multiply(9.63237170280645, x[0]))), np.multiply(np.add(np.multiply(np.multiply(x[1], 18.649830776786864), np.reciprocal(np.divide(-3.216528202444553, x[2]))), np.add(np.add(np.add(np.add(np.subtract(np.subtract(np.multiply(x[0], x[1]), np.power(0.6074640000624179, x[1])), np.power(np.power(1.6527811092644136, x[1]), 1.407884673731782)), np.add(np.remainder(np.subtract(0.6074640000624179, x[0]), 2.4566242613302007), np.subtract(np.multiply(x[0], x[1]), np.power(0.6074640000624179, x[1])))), np.remainder(np.sin(np.add(1.3867146563004298, np.multiply(1.295735309539861, x[0]))), 35.60407448498192)), np.multiply(np.multiply(np.remainder(np.subtract(6.984060689083957, np.multiply(1.295735309539861, x[0])), 5.66912256531004), x[0]), np.reciprocal(np.divide(2.196202080294855, x[2])))), np.add(np.add(np.add(np.add(np.cbrt(np.divide(x[0], x[1])), -1.0979916524655646), np.remainder(np.power(np.power(1.6527811092644136, x[1]), 1.407884673731782), np.subtract(np.multiply(x[0], x[1]), np.power(1.6527811092644136, x[1])))), np.multiply(np.multiply(np.multiply(x[0], x[1]), x[0]), np.reciprocal(np.divide(4.2910388267904604, x[2])))), 253.6732340675021))), np.divide(np.add(np.add(-82.29170605396563, np.multiply(x[0], x[1])), np.absolute(np.multiply(-9.996265568395703, x[0]))), 3.340770779277813)))
+
 
 
 def f3(x: np.ndarray) -> np.ndarray: 
-    return 0
+    return (np.arctan(((x[2] - (-1.9881484915868413)) / (np.arctan(4.51845980899566 + (1 / x[2]) % 19.132952747261935) * 2.6622227984463516) + 14.11482758558892)) * 2.6622227984463516) + (-((x[0] ** 2 * -2.0) + (3.503326125821749 / (1 / x[2])) + (x[1] * (x[1] ** 2))))
 
 
 def f4(x: np.ndarray) -> np.ndarray: 
-    return 0
+    return (np.cos(x[1]) / 2.2500185619926647 * 15.750936910045057) + np.sqrt((x[0] / -1.6703809608933167) - (-10.824923391590076))
+
 
 
 def f5(x: np.ndarray) -> np.ndarray: 
-    return 0
+    return -9.999955870603127e-12 * (np.power(x[0], x[1]) - 16.2417133569865)
 
 
 def f6(x: np.ndarray) -> np.ndarray: 
-    return 0
+    return ((x[1] - x[0]) * (2.7611329249887397 / 3.9754834296786434) + x[1]) * np.tanh(0.2708276964509544 ** np.arctan(-4.628376108847554))
+
 
 
 def f7(x: np.ndarray) -> np.ndarray: 
-    return 0
+    return  np.maximum(np.multiply(np.minimum(np.remainder(np.maximum(np.divide(x[1], x[0]), np.divide(x[0], np.maximum(x[1], -9.943834922872611))), np.remainder(np.divide(np.reciprocal(x[0]), np.subtract(x[1], -0.6122603029659004)), np.add(np.add(-9.943834922872611, 0.05938332891393472), np.add(x[0], -6.79238135420756)))), np.remainder(np.divide(np.subtract(np.exp(0.06076915069244038), np.divide(x[1], x[0])), np.add(np.sin(-8.228346998563147), np.divide(x[1], x[0]))), np.multiply(np.maximum(np.divide(2.486625643357174, 5.776652845226183), np.absolute(x[0])), np.add(np.negative(x[0]), np.sinh(-5.100900390254108))))), np.add(np.maximum(np.add(np.add(np.maximum(x[0], 7.982407377940877), -9.690808371156749), np.remainder(np.divide(x[1], x[0]), np.subtract(0.05938332891393472, 0.5466764907610671))), np.add(np.multiply(np.arctan(2.486625643357174), np.sin(-2.1837381300373977)), np.remainder(np.negative(x[0]), np.add(-6.79238135420756, 3.8572115202883204)))), np.minimum(np.add(np.remainder(x[0], np.cbrt(7.651910702072197)), np.remainder(np.maximum(x[1], -6.79238135420756), np.cbrt(5.528718708778168))), np.add(np.cbrt(3.8170973094190614), np.maximum(np.add(-9.943834922872611, -1.8573570053303747), np.add(-5.410905461618814, 4.825801675919273)))))), np.add(np.floor(np.add(np.remainder(np.remainder(x[0], np.cbrt(6.615145249452052)), np.add(-1.8866249975138452, np.multiply(-9.943834922872611, 5.528718708778168))), np.add(np.remainder(np.divide(x[1], 6.615145249452052), np.add(-9.690808371156749, -9.943834922872611)), np.remainder(x[0], np.cbrt(6.615145249452052))))), np.multiply(np.maximum(x[1], np.remainder(x[0], np.add(np.divide(x[1], 5.776652845226183), np.maximum(x[1], -9.943834922872611)))), np.multiply(x[0], np.subtract(np.subtract(np.subtract(-0.6122603029659004, -9.943834922872611), np.add(-9.943834922872611, -9.73309583113539)), np.add(np.add(-6.79238135420756, -5.100900390254108), np.add(x[0], -5.100900390254108)))))))
 
 
 def f8(x: np.ndarray) -> np.ndarray:
-    return 0
-
-functions = [f0, f1, f2, f3, f4, f5, f6, f7, f8]
+   return np.minimum(np.subtract(np.add(np.multiply(np.multiply(np.minimum(np.sinh(x[5]), np.sinh(x[5])), np.maximum(9.306755163948337, np.subtract(np.divide(x[0], x[0]), 8.17374013916162))), np.maximum(np.maximum(np.minimum(np.cosh(x[5]), 18.44238954070858), np.minimum(np.square(x[5]), 15.037856692023084)), np.add(np.minimum(17.6972179719256, np.square(x[5])), np.minimum(np.square(x[5]), np.absolute(x[5]))))), np.minimum(np.multiply(np.sinh(x[4]), np.maximum(37.0927078418996, x[3])), np.add(np.divide(np.maximum(np.sinh(x[5]), 8.680667755883558), -3.3592534823799656), np.multiply(np.multiply(np.sinh(x[4]), -4.070986083664829), 8.977972163360176)))), np.minimum(np.maximum(np.remainder(np.power(np.maximum(3.025150828938454, np.sin(x[1])), np.add(np.remainder(x[4], -4.190526875841163), 2.5852944295947005)), np.minimum(np.add(np.sinh(x[4]), np.sinh(x[5])), np.add(np.minimum(-6.530635069831985, x[2]), -15.751711154922196))), np.add(np.maximum(-165.52441838946697, np.divide(314.8402983836044, np.maximum(-4.933777461347297, x[3]))), np.divide(np.multiply(np.maximum(-9.174199129628121, x[3]), 97.8102957631067), -1.5537384634524345))), np.remainder(np.maximum(np.divide(100.01994107697637, np.subtract(x[3], 2.1332481808545545)), -32.77726592336975), np.multiply(np.minimum(np.multiply(36.7506213572169, np.cosh(x[5])), np.multiply(-4.109412935321963, np.maximum(-9.62084356692828, x[3]))), 17.361335511767116)))), np.subtract(np.multiply(np.add(np.multiply(np.minimum(np.add(np.maximum(-9.174199129628121, x[3]), np.sinh(x[5])), np.add(np.cosh(x[5]), np.cos(x[5]))), np.add(np.minimum(9.720508873329518, np.square(x[5])), np.minimum(4.583029845151287, np.absolute(x[5])))), np.multiply(np.maximum(np.divide(44.82781934259213, np.absolute(x[5])), np.add(np.absolute(x[5]), 6.695358044391064)), np.maximum(np.add(np.sinh(x[5]), np.cos(x[5])), 0.6087079909086857))), 8.680667755883558), np.minimum(np.square(np.maximum(-12.43159394773964, np.add(np.minimum(17.6972179719256, np.square(x[5])), np.minimum(np.square(x[5]), np.absolute(x[5]))))), np.square(np.minimum(np.maximum(np.maximum(np.sinh(x[5]), -2.1085570037841785), np.minimum(np.square(x[5]), np.absolute(x[5]))), np.add(np.minimum(np.sinh(x[5]), np.sinh(x[5])), -0.0013074377194559617))))))
 
 
-for i in range(9):
-    # Load the data
-    with np.load(f'data/problem_{i}.npz') as problem:
-        x = problem['x']
-        y = problem['y']
 
-    
-    # Test the functions and compute the mse
-    y_pred = functions[i](x)
-    mse = np.mean(np.square(y - y_pred))
-    print("Problem", i, "MSE:", mse)
 
 
 
